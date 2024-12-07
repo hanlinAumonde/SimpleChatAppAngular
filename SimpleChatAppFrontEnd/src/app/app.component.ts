d05e377cf57a71f76fcf5499914380836ccaa7c8
wuhanlin <841116184@qq.com>
2024-12-07 06:58:11 +0100

    Add Basic components

--- SimpleChatAppFrontEnd/src/app/app.component.ts
+++ SimpleChatAppFrontEnd/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./CommonComponents/header/header.component";
+import { MainComponentComponent } from "./ChatAppComponents/main-component/main-component.component";
+import { FooterComponent } from "./CommonComponents/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainComponentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SimpleChatAppFrontEnd';
+  
 }
